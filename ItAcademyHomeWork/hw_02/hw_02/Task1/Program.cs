1569b0ef1699fe57b21043dec2b576071b5198e8
Annet <annet.sle.va@gmail.com>
2019-10-13 23:30:22 +0300

    init hw_02

---
+++ ItAcademyHomeWork/hw_02/hw_02/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = 's';
+            int integer = 1214524365;
+            float single = 3.123F;
+            string date = "1992-12-01";
+            short shorty = 1;
+            sbyte bytes = default;
+            long longer = 12654646541564123;
+
+
+            // boxing/unboxing
+            object box = integer;
+            var unbox = (int)box;
+
+
+            // явное преобразование
+            string str = box.ToString();
+            DateTime datetime = DateTime.Parse(date);
+            decimal dec = Convert.ToDecimal(single);
+
+            // неявное преобразование
+            longer = integer;
+            integer = shorty;
+            shorty = bytes;
+
+
+
+        }
+    }
+}
---
+++ ItAcademyHomeWork/hw_02/hw_02/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object csharpObject = new Object();
+            object clrObject = new object();
+
+            Int32 csharpInteger = 1245;
+            int clrInteger = 4123;
+
+            String csharpString = "Is this c# type?";
+            string clrString = "Is this CLR type?";
+
+            Console.WriteLine(csharpObject.GetType());
+            Console.WriteLine(clrObject.GetType());
+            Console.WriteLine(csharpInteger.GetType());
+            Console.WriteLine(clrInteger.GetType());
+            Console.WriteLine(csharpString.GetType());
+            Console.WriteLine(clrString.GetType());
+            Console.ReadKey();
+        }
+    }
+}
